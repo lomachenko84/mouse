bb41fac656a8bfb35b186e148d039fd2957649be
Lomachenko84 <agent84@inbox.ru>
2023-02-12 23:56:46 +0300

    mouse 1.1

--- src/js/app.js
+++ src/js/app.js
@@ -1,25 +1,9 @@
-const obj = { name: 'мечник', health: 10, level: 2, attack: 80, defence: 40 }
+const button = document.querySelector(".button")
+console.log(button);
+const widget = document.querySelector(".widget")
 
-export function orderByProps(e, y) {
-let arr = [];
-    for (const prop in e) {
-        
-        let user = {
-            key: prop,
-            value: e[prop],
-            
-        }
-        arr.push(user);
-    }
-    
-    for (let i=y.length-1; i>=0; i--){
-        let elem = arr.find((obj) => obj.key === y[i])
-        let index = arr.indexOf(elem);
-        if (index != 0 && index !=-1){
-            arr.splice (index, 1);
-            arr.unshift(elem);
-        }
-        
-    }  
-    return arr;
-}
+
+button.addEventListener('click', function (e) {
+  e.preventDefault();
+  widget.classList.toggle("widget_active")
+})
